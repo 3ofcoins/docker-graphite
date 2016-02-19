acb3bcb035c08bfcc244be59d2c6c1de8fcfaa6b
Maciej Pasternacki <maciej@3ofcoins.net>
2016-02-19 16:17:48 +0100

    graphite-web: correctly start gunicorn

--- graphite-web.sh
+++ graphite-web.sh
@@ -7,4 +7,4 @@ python2.7 manage.py syncdb --noinput
 mkdir -p /opt/graphite/storage/log/webapp /opt/graphite/storage/var/webapp
 chown -R graphite:graphite /opt/graphite/storage/
 set -x
-exec /usr/local/bin/gunicorn_django -u graphite -g graphite -b 0.0.0.0:8080 /opt/graphite/webapp/graphite/settings.py
+exec /usr/local/bin/gunicorn -u graphite -g graphite -b 0.0.0.0:8080 wsgi:application
